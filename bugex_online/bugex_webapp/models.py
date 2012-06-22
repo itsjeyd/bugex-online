d07b90e0a79a3b939a92ddae06117c6c02daecf1
ili4 <ili4_s@yahoo.com>
2012-06-22 19:43:22 +0200

    Created Model classes for the Anonymous and Registered Users. Difference from the diagram: RegisteredUser inherits from AnonymousUser (as discussed), abstract User does not exist.

--- bugex_online/bugex_webapp/models.py
+++ bugex_online/bugex_webapp/models.py
@@ -13,3 +13,34 @@ Authors: Amir Baradaran
 
 from django.db import models
 
+class AnonymousUser(models.Model):
+    registration_date = models.DateField()
+    email_address = models.EmailField()
+    
+    def __unicode__(self):
+        return '{0}'.format(self.email_address)
+    
+    def register(self):
+        '''Create a registered user
+        '''
+        pass
+    
+    def update_email(self, new_email):
+        #self.email_address = new_email
+        pass
+    
+    def update_password(self):
+        pass
+    
+class RegisteredUser(AnonymousUser):
+    password = models.CharField() #size?
+    
+    def generate_password(self):
+        pass
+    
+    
+    
+    
+    
+
+
