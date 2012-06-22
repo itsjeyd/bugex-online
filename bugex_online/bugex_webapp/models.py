8928b3d7b4db5c094e780eb946fd9b12b7c3a382
Tim Krones <t.krones@gmx.net>
2012-06-22 19:55:43 +0200

    Add CodeArchive model

--- bugex_online/bugex_webapp/models.py
+++ bugex_online/bugex_webapp/models.py
@@ -26,3 +26,13 @@ class UserRequest(models.Model):
         return u'{0}: {1}'.format(self.token, self. test_case)
 
 
+class CodeArchive(models.Model):
+    EXTENSION_CHOICES = (('jar', 'Type of archive: jar'),
+                         ('zip', 'Type of archive: zip'))
+    name = models.CharField()
+    extension = models.CharField(choices=EXTENSION_CHOICES) # This is called "type" in class diagram
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
+
+
