941f297781f25b7fb96437f52dfd2d9c45c65b28
Tim Krones <t.krones@gmx.net>
2012-06-22 19:55:15 +0200

    Add UserRequest model

--- bugex_online/bugex_webapp/models.py
+++ bugex_online/bugex_webapp/models.py
@@ -13,3 +13,16 @@ Authors: Amir Baradaran
 
 from django.db import models
 
+class UserRequest(models.Model):
+    user = models.ForeignKey('User')
+    code_archive = models.OneToOneField('CodeArchive')
+    test_case = models.OneToOneField('TestCase')
+    token = models.CharField()
+    request_folder = models.CharField()
+    status = models.IntegerField()
+    result = models.OneToOneField('BugExResult')
+
+    def __unicode__(self):
+        return u'{0}: {1}'.format(self.token, self. test_case)
+
+
