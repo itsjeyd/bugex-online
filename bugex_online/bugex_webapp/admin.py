dfc11a424cb586bfbae925886182e4c026b5fe75
Peter Michael Stahl <pemistahl@googlemail.com>
2012-06-26 14:42:00 +0200

    Registering missing models in admin.py

--- bugex_online/bugex_webapp/admin.py
+++ bugex_online/bugex_webapp/admin.py
@@ -13,15 +13,19 @@ Authors: Amir Baradaran
 
 from django.contrib import admin
 
-from bugex_webapp.models import SourceFile, ClassFile, Line, \
-    MethodElement, FieldElement, ClassElement, Folder, BugExResult, Fact
+from bugex_webapp.models import UserRequest, CodeArchive, TestCase, BugExResult
+from bugex_webapp.models import Fact, Folder, SourceFile, ClassFile, Line
+from bugex_webapp.models import MethodElement, FieldElement, ClassElement
 
+admin.site.register(UserRequest)
+admin.site.register(CodeArchive)
+admin.site.register(TestCase)
+admin.site.register(BugExResult)
+admin.site.register(Fact)
+admin.site.register(Folder)
 admin.site.register(SourceFile)
 admin.site.register(ClassFile)
 admin.site.register(Line)
 admin.site.register(MethodElement)
 admin.site.register(FieldElement)
 admin.site.register(ClassElement)
-admin.site.register(Folder)
-admin.site.register(BugExResult)
-admin.site.register(Fact)
