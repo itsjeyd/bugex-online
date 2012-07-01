1a6b39de1e262c9adf7c52ada6681c2db4404e99
Peter Michael Stahl <pemistahl@googlemail.com>
2012-07-01 13:15:29 +0200

    Undoing formtools support again as it cannot handle file uploads.

--- bugex_online/bugex_online/settings.py
+++ bugex_online/bugex_online/settings.py
@@ -86,7 +86,7 @@ MEDIA_ROOT = '{0}/uploads'.format(ROOT_PATH)
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -157,7 +157,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.formtools',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- bugex_online/bugex_webapp/urls.py
+++ bugex_online/bugex_webapp/urls.py
@@ -15,10 +15,6 @@ from django.conf.urls import patterns, include, url
 
 from bugex_webapp.views import MainPageView, HowToPageView, ResultsPageView
 from bugex_webapp.views import DeletePageView, UserPageView
-from bugex_webapp.previews import BugExFormPreview
-from bugex_webapp.forms import EmailBaseForm, EmailPasswordBaseForm
-from bugex_webapp.forms import UserRequestForm, AdditionalTestCaseForm
-from bugex_webapp.forms import ChangeEmailForm
 
 urlpatterns = patterns('',
     url(r'main/$', MainPageView.as_view(), name='main-page'),
@@ -26,11 +22,4 @@ urlpatterns = patterns('',
     url(r'results/$', ResultsPageView.as_view(), name='results-page'),
     url(r'delete/$', DeletePageView.as_view(), name='delete-page'),
     url(r'user/$', UserPageView.as_view(), name='user-page'),
-
-    # These URLs are for checking the forms via the formtools app.
-    url(r'form/emailbase$', BugExFormPreview(EmailBaseForm)),
-    url(r'form/emailpasswordbase$', BugExFormPreview(EmailPasswordBaseForm)),
-    url(r'form/userrequest$', BugExFormPreview(UserRequestForm)),
-    url(r'form/additionaltestcase$', BugExFormPreview(AdditionalTestCaseForm)),
-    url(r'form/changeemail$', BugExFormPreview(ChangeEmailForm)),
 )
