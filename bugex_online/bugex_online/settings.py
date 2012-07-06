804b17a64660fe613512959d101dbf7b96e524ca
ili4 <ili4_s@yahoo.com>
2012-07-06 15:01:40 +0200

    Added settings for the email notification

--- bugex_online/bugex_online/settings.py
+++ bugex_online/bugex_online/settings.py
@@ -192,3 +192,9 @@ LOGGING = {
         },
     }
 }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bugexonline@gmail.com'
+EMAIL_HOST_PASSWORD = 'eniln0xeguB'
