9f2831c0bf3ab875099b0f2936eb3f0939aa6a4c
ili4 <ili4_s@yahoo.com>
2012-07-06 15:01:03 +0200

    Added email notification messages

--- bugex_online/bugex_webapp/__init__.py
+++ bugex_online/bugex_webapp/__init__.py
@@ -19,9 +19,27 @@ FAILED = 5
 FINISHED = 6
 DELETED = 7
 
+#BugEx result file xml node names
 FACT_NODE = './/fact'
 CLASS_NODE = 'className'
 LINE_NODE = 'lineNumber'
 METHOD_NODE = 'methodName'
 EXPL_NODE = 'explanation'
 TYPE_NODE = 'factType'
+
+#messages for user email notification, mapped to different UserRequest statuses
+NOTIFY_HEADER_FOOTER = 'Dear BugEx Online user,\n\n%s\n\nBest,\nBugEx Online Team'
+NOTIFICATIONS = {
+                 1:('Input files successfully received',
+                    'The input you\'ve submitted to BugEx Online has been ' +
+                    'successfully uploaded and is being processed.'), 
+                 3:('Your request could not be processed',
+                     'Unfortunately your request could not be processed.'),
+                 5:('Your request could not be processed',
+                     'Unfortunately your request could not be processed.'),
+                 6:('Your BugEx result is available',
+                    'BugEx has finished processing your request. You can ' + 
+                    'access the result here: %s.'),
+                 7:('Your BugEx result has been deleted',
+                    'You have successfully deleted your BugEx result files.')
+                 }
