a9fe7a10abf80aac1e2338603b2e9f1200066f39
Peter Michael Stahl <pemistahl@googlemail.com>
2012-07-11 21:14:33 +0200

    Removing another bug in dictionary in show_bugex_result view.

--- bugex_online/bugex_webapp/views.py
+++ bugex_online/bugex_webapp/views.py
@@ -375,8 +375,9 @@ def show_bugex_result(request, token):
         fact_dict = {}
         for fact_type in Fact.FACT_TYPES:
             fact_dict[fact_type[0]] = []
-            for fact in user_request.result.fact_set.all():
-                fact_dict[fact.fact_type].append(fact)
+
+        for fact in user_request.result.fact_set.all():
+            fact_dict[fact.fact_type].append(fact)
 
         template_context = {
             'fact_dict': fact_dict,
