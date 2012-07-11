c3223b757e44f9ccac6ff531b97a6992289f0b39
ChrisMonogios <chr_mon_@hotmail.com>
2012-07-11 12:57:14 +0200

    changes.... peter sent me the dictionary form

--- bugex_online/bugex_online/settings.py
+++ bugex_online/bugex_online/settings.py
@@ -165,8 +165,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'bugex_webapp',
-    'captcha'
+    #'bugex_webapp',
+    #'captcha'
 )
 
 # A sample logging configuration. The only tangible logging
