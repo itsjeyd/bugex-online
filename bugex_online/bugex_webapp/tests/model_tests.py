8052d6091303be542aa3e94fd6b63fa1d952ea2d
Tim Krones <t.krones@gmx.net>
2012-07-11 23:38:04 +0200

    Remove UserRequestConstructorTest and UserRequestUpdateStatusTest classes: UserRequest methods and properties work differently now, so better implement tests for them from scratch

--- bugex_online/bugex_webapp/tests/model_tests.py
+++ bugex_online/bugex_webapp/tests/model_tests.py
@@ -22,63 +22,6 @@ from bugex_webapp.models import CodeArchive
 from bugex_webapp.models import UserRequest
 
 
-class UserRequestConstructorTest(TestCase):
-    """ Tests for the static constructor of the UserRequest model
-    (UserRequest.new)
-    """
-
-    test_case = 'FooTest.testFoo'
-    code_archive = 'archive.jar'
-
-    def setUp(self):
-        user = User.objects.create(email='user@example.com')
-        ur = UserRequest.new(user, self.test_case, self.code_archive)
-        ur.save()
-
-    def test_token(self):
-        ur = UserRequest.objects.get(id=1)
-        self.assertEqual(len(str(ur.token)), 36)
-
-    def test_test_case(self):
-        ur = UserRequest.objects.get(id=1)
-        self.assertEqual(ur.test_case.name, 'FooTest.testFoo')
-
-    def test_code_archive(self):
-        ur = UserRequest.objects.get(id=1)
-        self.assertEqual(ur.code_archive.name, 'archive.jar')
-
-    def test_code_archive_format(self):
-        ur = UserRequest.objects.get(id=1)
-        self.assertEqual(ur.code_archive.archive_format, 'JAR')
-
-    def test_status(self):
-        ur = UserRequest.objects.get(id=1)
-        self.assertEqual(ur.status, UserRequestStatus.PENDING)
-
-    def test_folder(self):
-        ur = UserRequest.objects.get(id=1)
-        path = os.path.join(settings.MEDIA_ROOT, 'user_1', ur.token)
-        self.assertEqual(ur.folder, path)
-
-
-class UserRequestUpdateStatusTest(TestCase):
-    """  """
-    test_case = 'FooTest.testFoo'
-    code_archive = 'archive.jar'
-
-    def setUp(self):
-        user = User.objects.create(email='user@example.com')
-        ur = UserRequest.new(user, self.test_case, self.code_archive)
-        ur.save()
-
-    def test_update_status(self):
-        ur = UserRequest.objects.get(id=1)
-        old_status = ur.status
-        ur.update_status(UserRequestStatus.VALID)
-        self.assertNotEqual(old_status, ur.status)
-        self.assertEqual(UserRequestStatus.VALID, ur.status)
-
-
 class CodeArchiveTest(TestCase):
     """
     Tests for methods + properties of the CodeArchive model
