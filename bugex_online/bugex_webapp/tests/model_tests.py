d6cf2cdcfeb0f646d9412ac4ac36059f1b247da3
Tim Krones <t.krones@gmx.net>
2012-07-11 23:18:31 +0200

    Add first version of tests using fixtures for CodeArchive properties

--- bugex_online/bugex_webapp/tests/model_tests.py
+++ bugex_online/bugex_webapp/tests/model_tests.py
@@ -77,3 +77,40 @@ class UserRequestUpdateStatusTest(TestCase):
         ur.update_status(UserRequestStatus.VALID)
         self.assertNotEqual(old_status, ur.status)
         self.assertEqual(UserRequestStatus.VALID, ur.status)
+
+
+class CodeArchiveTest(TestCase):
+    """
+    Tests for methods + properties of the CodeArchive model
+    """
+    fixtures = ['test_data.json']
+
+    def test_name(self):
+        """ Test the 'name' property of CodeArchive """
+        code_archive = CodeArchive.objects.get(archive_format='JAR')
+        self.assertEqual(
+            code_archive.name,
+            'failing-program-0.0.2-SNAPSHOT-jar-with-dependencies.jar')
+
+    def test_path(self):
+        """
+        Test the 'path' property of CodeArchive, which should return
+        the absolute path of the physical archive file
+        """
+        code_archive = CodeArchive.objects.get(archive_format='JAR')
+        self.assertTrue(code_archive.path.endswith(
+            'uploads/user_2/f99db44e-c841-444b-977b-ccc9baa11027/' \
+            'failing-program-0.0.2-SNAPSHOT-jar-with-dependencies.jar'))
+
+    def test_absolute_extracted_path(self):
+        """
+        Test the 'absolute_extracted_path' property of CodeArchive,
+        which should return the absolute path of the root folder of
+        the archive contents. We are calling this folder
+        'tmp_extracted'.
+        """
+        code_archive = CodeArchive.objects.get(archive_format='JAR')
+        self.assertTrue(code_archive.absolute_extracted_path.endswith(
+            'uploads/user_2/f99db44e-c841-444b-977b-ccc9baa11027/' \
+            'tmp_extracted'))
+
