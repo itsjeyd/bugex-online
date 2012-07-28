1ebc68d82d5959b500704ad4748ad3d3ae04e7f5
Frederik Leonhardt <frederik.leonhardt@googlemail.com>
2012-07-28 13:29:08 +0200

    Indentation gone wild, 2nd cut.

--- bugex_online/bugex_webapp/templatetags/custom_tags.py
+++ bugex_online/bugex_webapp/templatetags/custom_tags.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.simple_tag
 def settings_value(name):
-"""
-This Tag allows to access values from the configuration file in the templates.
-"""
+    """
+    This Tag allows to access values from the configuration file in the templates.
+    """
     try:
         return settings.__getattr__(name)
     except AttributeError:
